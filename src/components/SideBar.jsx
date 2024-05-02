042291be06a9b83e3a95c24c93eff13bfe6e8097
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-02 15:27:42 +0100

    avancée sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { globalState1 } from "../ContextComp";
+
+const SideBar = () => {
+  return (
+    <div>SideBar</div>
+  )
+}
+
+export default SideBar
