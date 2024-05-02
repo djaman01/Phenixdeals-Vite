31f5621866ffee489be54f7b0e8fe7a93f065f06
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-02 16:49:37 +0100

    bonne avancée sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { globalState1 } from "../ContextComp";
 import { hamburger } from "../assets/icons";
 import { logoPhenix } from "../assets/images";
-
+import SideBar from "./SideBar";
 const Header = () => {
+  const { openSidebar, setOpenSidebar } = globalState1();
+
   return (
     <header className="padding-x w-full py-1">
       <nav className="max-container flex items-center justify-between">
@@ -21,13 +24,18 @@ const Header = () => {
         </div>
 
         <div className="hidden max-lg:block">
-          <img
-            src={hamburger}
-            alt="menu-hamburger"
-            width={25}
-            height={25}
-            className="cursor-pointer"
-          />
+          {openSidebar === false ? (
+            <img
+              src={hamburger}
+              alt="menu-hamburger"
+              width={25}
+              height={25}
+              className="cursor-pointer"
+              onClick={() => setOpenSidebar(true)}
+            />
+          ) : (
+            <SideBar className="lg:hidden" /> //Pour que si on oublie de la fermer, elle n'appraisse pas en grand écran
+          )}
         </div>
       </nav>
     </header>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,32 @@
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { globalState1 } from "../ContextComp";
+import { Link } from "react-router-dom";
+import { IoHomeOutline } from "react-icons/io5";
 
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+  const { openSidebar, setOpenSidebar } = globalState1();
 
-export default SideBar
+  return (
+    <div style={{ display: "flex", height: "100%" }}>
+      <Sidebar
+        backgroundColor="#ededed"
+        breakPoint="all"
+        onBackdropClick={() => setOpenSidebar(false)} //permet de mettre faux quand on clique hors de la sidebar
+        toggled={openSidebar}
+      >
+        <Menu>
+          <MenuItem
+            component={<Link to="/" />}
+            icon={<IoHomeOutline size={18} />}
+          >
+            Accueil
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+
+      
+    </div>
+  );
+};
+
+export default SideBar;
