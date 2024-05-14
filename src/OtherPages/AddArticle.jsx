80320033db68b8d29d274de1a01af148248c8d9e
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-14 01:04:25 +0100

    ajout AddArticles

--- src/OtherPages/AddArticle.jsx
+++ src/OtherPages/AddArticle.jsx
@@ -46,90 +46,95 @@ const AddArticle = () => {
       <Header />
       <div className="bg-[#e8e8e8]">
         <div className="flex h-screen flex-col items-center justify-center">
-          <div className="w-full max-w-md rounded-lg border border-gray-300 bg-white p-6 shadow-md">
+          <div className="w-3/4 rounded-lg border border-gray-300 bg-white p-6 shadow-md">
             <h2 className="mb-4 text-2xl font-bold text-gray-900">
               Add Article
             </h2>
+            <div className="grid grid-cols-2 gap-10 ">
+              <form className="flex flex-col">
+                <input
+                  required
+                  type="text"
+                  className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  placeholder="Type de l'article"
+                  value={type}
+                  onChange={(e) => setType(e.target.value)}
+                />
+                <input
+                  required
+                  type="text"
+                  className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  placeholder="Auteur"
+                  value={auteur}
+                  onChange={(e) => setAuteur(e.target.value)}
+                />
+                <input
+                  required
+                  type="text"
+                  className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  placeholder="Info Article"
+                  value={infoArticle}
+                  onChange={(e) => setInfoArticle(e.target.value)}
+                />
+                <input
+                  required
+                  type="text"
+                  className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  placeholder="Prix"
+                  value={prix}
+                  onChange={(e) => setPrix(e.target.value)}
+                />
+                <input
+                  required
+                  type="text"
+                  className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  placeholder="Achat ou Dépôt"
+                  value={etat}
+                  onChange={(e) => setEtat(e.target.value)}
+                />
+                <input
+                  required
+                  type="text"
+                  className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  placeholder="Code Article"
+                  value={code}
+                  onChange={(e) => setCode(e.target.value)}
+                />
 
-            <form className="flex flex-col">
-              <input
-                required
-                type="text"
-                className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                placeholder="Type de l'article"
-                value={type}
-                onChange={(e) => setType(e.target.value)}
-              />
-              <input
-                required
-                type="text"
-                className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                placeholder="Auteur"
-                value={auteur}
-                onChange={(e) => setAuteur(e.target.value)}
-              />
-              <input
-                required
-                type="text"
-                className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                placeholder="Info Article"
-                value={type}
-                onChange={(e) => setInfoArticle(e.target.value)}
-              />
-              <input
-                required
-                type="text"
-                className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                placeholder="Prix"
-                value={prix}
-                onChange={(e) => setPrix(e.target.value)}
-              />
-              <input
-                required
-                type="text"
-                className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                placeholder="Achat ou Dépôt"
-                value={etat}
-                onChange={(e) => setEtat(e.target.value)}
-              />
-              <input
-                required
-                type="text"
-                className="mb-4 rounded-md border border-gray-400 bg-gray-100 p-2 text-gray-900 transition duration-150 ease-in-out focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                placeholder="Code Article"
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-              />
+                {/* Dropping the image will store it in the imageUrl state variable */}
+                <Dropzone
+                  onDrop={(acceptedFiles) => setImageUrl(acceptedFiles[0])}
+                >
+                  {({ getRootProps, getInputProps }) => (
+                    <div
+                      className="flex h-16 items-center justify-center border border-slate-400"
+                      {...getRootProps()}
+                    >
+                      <input {...getInputProps()} />
+                      <p>Drag & drop an image here, or click to select one</p>
+                    </div>
+                  )}
+                </Dropzone>
 
-              {/* Dropping the image will store it in the imageUrl state variable */}
-              <Dropzone
-                onDrop={(acceptedFiles) => setImageUrl(acceptedFiles[0])}
-              >
-                {({ getRootProps, getInputProps }) => (
-                  <div
-                    className="flex h-16 items-center justify-center border border-slate-400"
-                    {...getRootProps()}
-                  >
-                    <input {...getInputProps()} />
-                    <p>Drag & drop an image here, or click to select one</p>
-                  </div>
+                <button
+                  type="submit"
+                  className="mt-4 rounded-md bg-gradient-to-r from-indigo-500 to-blue-500 px-4 py-2 font-bold text-white transition duration-150 ease-in-out hover:bg-indigo-600 hover:to-blue-600"
+                  onClick={handleSubmit}
+                >
+                  Send Article
+                </button>
+              </form>
+              <div className="flex items-center border border-gray-400 ">
+                {/*Pour que l'image n'apparaisse que si imageURL contient une image */}
+                {imageUrl && (
+                  <img
+                    src={URL.createObjectURL(imageUrl)}
+                    alt="image uploaded"
+                    className="max-w-full"
+                  />
                 )}
-              </Dropzone>
-
-              <button
-                type="submit"
-                className="mt-4 rounded-md bg-gradient-to-r from-indigo-500 to-blue-500 px-4 py-2 font-bold text-white transition duration-150 ease-in-out hover:bg-indigo-600 hover:to-blue-600"
-                onClick={handleSubmit}
-              >
-                Send Article
-              </button>
-            </form>
-          </div>
-          <div>
-            <img
-              src={imageUrl && URL.createObjectURL(imageUrl)}
-              alt="image uploaded"
-            />
+              </div>
+            </div>
           </div>
         </div>
       </div>
