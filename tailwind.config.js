8f1f6d74d038234a9c35503c431e3da4710e6daa
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-17 17:31:26 +0100

    avancée style card

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {//utiliser shadow-custom, car tailwind ajoute automatiquement le nom de la propriété css
+        'custom':'0 0 20px 15px rgba(0, 0, 0, 0.1)',
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
