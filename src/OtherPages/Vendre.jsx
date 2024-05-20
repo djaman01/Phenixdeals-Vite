ee79b831d7320f9dae60497e6dd1b64930ee9779
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-05-20 14:00:20 +0100

    fin Vendre.jsx

--- src/OtherPages/Vendre.jsx
+++ src/OtherPages/Vendre.jsx
@@ -11,20 +11,16 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 import ContactForm from "../components/ContactForm";
 
-
-
 const Vendre = () => {
-
-  
-useEffect(() => {
-  Aos.init({
-    once: true,
-    offset: 100,
-    duration: 1000,
-    easing: 'ease-in-out',
-    // delay: 100,
+  useEffect(() => {
+    Aos.init({
+      once: true,
+      offset: 100,
+      duration: 1000,
+      easing: "ease-in-out",
+      // delay: 100,
+    });
   });
-})
 
   return (
     <>
@@ -33,24 +29,22 @@ useEffect(() => {
       </header>
 
       <main>
-        
-          <div className="mt-5 ">
-            <h1 className="montserrat-regular text-center text-[45px] text-blue-700 max-lg:mt-4">
-              Contactez-nous
-            </h1>
-          </div>
+        <div>
+          <h1 className="montserrat-regular text-center text-[45px] text-blue-700 max-lg:mt-4">
+            Contactez-nous
+          </h1>
+        </div>
 
-          <div className="mx-auto mb-10 flex h-2 w-24 overflow-hidden rounded">
-            {/* flex-1 = the element will grow to fill available space along the main axis */}
-            <div className=" bg-[#69f0ae] flex-1"></div>
-            <div className=" bg-[#00e676] flex-1"></div>
-            <div className=" bg-[#00c853] flex-1"></div>
-          </div>
+        <div className="mx-auto mb-10 flex h-2 w-24 overflow-hidden rounded">
+          {/* flex-1 = the element will grow to fill available space along the main axis */}
+          <div className=" flex-1 bg-[#00e676]"></div>
+        </div>
 
-          <div className="my-16 flex justify-center gap-20 max-lg:flex-col max-lg:items-center">
+        <div className="grid grid-cols-2">
+          <div className="ml-32 grid grid-cols-2 grid-rows-3 gap-8">
             <div
               data-aos="flip-left"
-              className="flex h-60 w-72 flex-col items-center justify-center rounded-lg border border-gray-400 bg-gray-100"
+              className="flex h-60 w-full flex-col items-center justify-center rounded-lg border border-gray-400 bg-blue-100"
             >
               <div className=" mb-4 mt-[-45px] flex justify-center rounded-full bg-red-400 px-3 py-3">
                 <IoMailOutline color="white" size={20} />
@@ -64,8 +58,8 @@ useEffect(() => {
             </div>
 
             <div
-              data-aos="flip-right"
-              className=" flex h-60 w-72 flex-col items-center justify-center rounded-lg border border-gray-400 bg-gray-100 text-gray-700"
+              data-aos="flip-left"
+              className=" flex h-60 w-full flex-col items-center justify-center rounded-lg border border-gray-400 bg-blue-100 text-gray-700"
             >
               <div className=" mb-4 mt-[-45px] flex justify-center rounded-full bg-red-400 px-3 py-3">
                 <IoShareSocialOutline color="white" size={20} />
@@ -78,8 +72,8 @@ useEffect(() => {
             </div>
 
             <div
-              data-aos="flip-up"
-              className="flex h-60 w-72 flex-col items-center justify-center rounded-lg border border-gray-400 bg-gray-100"
+              data-aos="flip-left"
+              className="flex h-60 w-full flex-col items-center justify-center rounded-lg border border-gray-400 bg-blue-100"
             >
               <div className=" mb-4 mt-[-45px] flex justify-center rounded-full bg-red-400 px-3 py-3">
                 <CiPhone color="white" size={20} />
@@ -87,18 +81,14 @@ useEffect(() => {
               <h2 className=" mb-3 text-2xl font-semibold text-gray-700">
                 Téléphone
               </h2>
-              <p className="font-palanquin text-xl text-gray-500">
-                /
-              </p>
+              <p className="font-palanquin text-xl text-gray-500">/</p>
             </div>
-
-          
           </div>
 
-          <div>
+          <div data-aos="fade-left">
             <ContactForm />
           </div>
-      
+        </div>
       </main>
     </>
   );
