0e47f29f6094920643d4354719e0bdb2bc3c04b0
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-06-23 18:45:08 +0100

    ajout images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,6 @@
 import logoPhenix from "./phenix-nobg.png"
+import phenixAchat from "./phenix-achat.jpg"
+import phenixVendre from "./phenix-vendre.jpg"
+import phenixPub from "./phenix-pub.jpg"
 
-export {logoPhenix}
+export {logoPhenix, phenixAchat, phenixVendre, phenixPub}
