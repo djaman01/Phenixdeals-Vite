610b88364f73ae9420154cb9eae86decdaa28294
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-06-25 14:04:47 +0100

    fin carousel

--- src/HomeSections/ScrollPage.jsx
+++ src/HomeSections/ScrollPage.jsx
@@ -1,17 +1,23 @@
 import { phenixAchat, phenixPub, phenixVendre } from "../assets/images";
-
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
 
 function Arrow(props) {
   const { className, style, onClick } = props;
   return (
-    
     <div
       className={className}
-      style={{ ...style, display: "block", background: "black", borderRadius: "100%", cursor: "pointer", paddingTop: "0.5px" }}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        borderRadius: "100%",
+        cursor: "pointer",
+        paddingTop: "0.5px",
+      }}
       onClick={onClick}
     />
   );
@@ -30,9 +36,9 @@ const ScrollPage = () => {
     autoplay: true,
     autoplaySpeed: 5000,
     centerMode: true,
+    centerPadding: "60px",
     draggable: true,
     pauseOnFocus: true,
-    centerPadding: "60px",
     responsive: [
       {
         breakpoint: 1024,
@@ -62,32 +68,37 @@ const ScrollPage = () => {
   };
 
   return (
-      <div>
-        <Slider {...settings} className="w-[1000px] mx-auto h-[700px]">
-          <div className="px-2">
+    <div>
+      <Slider {...settings} className="mx-auto h-[700px] w-[1000px]">
+        <Link to="./concept">
+          <div className="h-[700px] px-2">
             <img
               src={phenixVendre}
               alt="phenix vendre"
-              className="object-fit h-[700px] w-[1000px] "
+              className="h-full w-full object-fill "
             />
           </div>
-          <div className="px-2">
+        </Link>
+        <Link to="./concept">
+          <div className="h-[700px] px-2">
             <img
               src={phenixAchat}
               alt="phenix Achat"
-               className="object-fit h-[700px] w-[1000px] "
+              className="h-full w-full object-fill "
             />
           </div>
-          <div className="px-2">
+        </Link>
+        <Link to='/vendre'>
+          <div className="h-[700px] px-2">
             <img
               src={phenixPub}
               alt="phenix pub"
-               className="object-fit h-[700px] w-[1000px] "
+              className="h-full w-full object-fill "
             />
           </div>
-        </Slider>
-      </div>
-  
+        </Link>
+      </Slider>
+    </div>
   );
 };
 
