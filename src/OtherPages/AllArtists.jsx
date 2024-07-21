ab19ab53e1c7d66d6bf9af869d27c3daa2dd8742
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-07-21 18:55:22 +0100

    fin commentaire AllArtists

--- src/OtherPages/AllArtists.jsx
+++ src/OtherPages/AllArtists.jsx
@@ -24,6 +24,20 @@ const AllArtists = () => {
       });
   }, []);
 
+  //Array Operation: Group artists by the first letter of their name (les noms des artistes sont dans l'array artists)
+  const groupedArtists = artists.reduce((acc, curr) => {// curr = élément actuel de l'array 'artists' sur lequel la fonction va être appliquée
+    const firstLetter = curr.auteur[0].toUpperCase(); // Accède à la 1ère lettre du nom de l'artiste qui a pour property 'auteur' (voir model-doc.js dans back-end)
+
+    // Si acc[firstLetter] n'existe pas encore, l'initialiser avec un tableau vide, sinon passe à l'étape suivante d'ajout du nom de l'artiste dans l'array déjà existante
+    !acc[firstLetter] && (acc[firstLetter] = []); // Si acc["A"] undefined alors acc["A"] = [] est exécuté, donc acc devient { "A": [] }.
+
+    //acc["A"]=[]; donc on ajoute le nom de l'artiste à l'array de "A" => { "A": [{auteur: "Abbas Saladi"}] }.
+    acc[firstLetter].push(curr);
+
+    // Retourner l'accumulateur pour la prochaine itération
+    return acc;
+  }, {}); // !!! {} On initialise l'accumulateur comme un objet vide
+
   return (
     <>
       <Header />
@@ -32,16 +46,24 @@ const AllArtists = () => {
           <h1>Tous les Artistes</h1>
         </div>
 
-        <div className="text-center my-20">
-        { (artists) ? (
-          <ul>
-            {artists.map((e, index) => (
-              <li key={index}>{e.auteur}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>{error || "Chargement en cours..."}</p>
-        )}
+        <div className="text-center">
+          {error ? (
+            <p>Error: {error}</p>
+          ) : (
+            Object.keys(groupedArtists) //Object.keys permet de créer une array avec les properties = keys de l'objet groupedArtists crée précédemment avec .reduce
+              .sort() //Même si tout est déjà dans l'ordre, ça ajoute une confirmation
+              .map((letter) => (
+                <div key={letter} className="mb-5">
+                  <h2 className="text-2xl">{letter}</h2>
+                  {/* On fait <ul> pour ne pas qu'il y ait de point avant chaque nom d'artiste */}
+                  <ul>
+                    {groupedArtists[letter].map((e, index) => ( //On refait un .map sur groupedArtists car avant on mapper sur une array avec que les key, alors que là on a besoin de la value auteur:"" pour chaque Key c'est pourquoi on fait groupedArtists[letter]
+                      <li key={index}>{e.auteur}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))
+          )}
         </div>
       </div>
       <Footer />
@@ -50,3 +72,4 @@ const AllArtists = () => {
 };
 
 export default AllArtists;
+
