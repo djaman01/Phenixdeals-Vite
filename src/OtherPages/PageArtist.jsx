fabc3a8f3ae4925eafcfb64ea43951e64047c73e
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-07-27 21:06:59 +0100

    Oprimisation PageArtist et RangeGrid

--- src/OtherPages/PageArtist.jsx
+++ src/OtherPages/PageArtist.jsx
@@ -6,17 +6,16 @@ import axios from "axios";
 import RangeGrid from "../components/RangeGrid";
 
 const PageArtist = () => {
-  const { auteur } = useParams();
+  const { auteur } = useParams(); //On tire le paramètre à la fin de l'url, qui est le nom de l'artiste, définit dans le component AllArtists, dans le lien quand on clique sur le nom de l'artiste
 
-  const [oeuvre, setOeuvre] = useState([]);
+  const [oeuvres, setOeuvres] = useState([]);
   const [errorText, setErrorText] = useState("");
 
   useEffect(() => {
     axios
       .get(`http://localhost:3005/pageArtist/${auteur}`)
       .then((response) => {
-        setOeuvre(response.data);
-        setFilteredArticles(response.data); // Initialize with all artworks
+        setOeuvres(response.data);
         console.log("Oeuvre Fetched", response.data);
       })
       .catch((error) => {
@@ -28,70 +27,16 @@ const PageArtist = () => {
         setErrorText("An error occurred while fetching data");
       });
   }, [auteur]);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "instant",
-    });
-  };
-
-  const [prixMin, setPrixMin] = useState("");
-  const [prixMax, setPrixMax] = useState("");
-
-  const handlePrixMin = (e) => setPrixMin(e.target.value);
-  const handlePrixMax = (e) => setPrixMax(e.target.value);
-
-  const [filteredArticles, setFilteredArticles] = useState([]);
-
-  const handleFilter = () => {
-    
-    const minPrice = prixMin
-      ? parseFloat(prixMin.replace(/\s+/g, ""))
-      : -Infinity;
-
-    const maxPrice = prixMax
-      ? parseFloat(prixMax.replace(/\s+/g, ""))
-      : Infinity;
   
-    const filtered = oeuvre.filter((e) => {
-      const price = parseFloat(e.prix.replace(/\s+/g, ""));
-      return price >= minPrice && price <= maxPrice;
-    });
-  
-    // Sort filtered articles by price in ascending order
-    const sortedFiltered = filtered.sort((a, b) => {
-      const priceA = parseFloat(a.prix.replace(/\s+/g, ""));
-      const priceB = parseFloat(b.prix.replace(/\s+/g, ""));
-      return priceA - priceB;
-    });
-  
-    setFilteredArticles(sortedFiltered);
-  };
-  
- 
-  const handleReset = () => {
-    setPrixMin("");
-    setPrixMax("");
-    setFilteredArticles(oeuvre);
-  };
 
   return (
     <>
       <Header />
 
       <RangeGrid
-        title="Toutes les oeuvres de l'Artiste"
-        placeholder="Prix du Tableau"
+        title= {`Toutes les oeuvres de ${auteur}`}
+        allValues={oeuvres}
         error={errorText}
-        minValue={prixMin}
-        maxValue={prixMax}
-        ChangePrixMin={handlePrixMin}
-        ChangePrixMax={handlePrixMax}
-        filteredArticles={filteredArticles}
-        onFilter={handleFilter}
-        onClick={scrollToTop}
-        reset={handleReset}
       />
       <Footer />
     </>
--- src/components/RangeGrid.jsx
+++ src/components/RangeGrid.jsx
@@ -1,19 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SlMagnifier } from "react-icons/sl";
 import { Link } from "react-router-dom";
 
 const RangeGrid = ({
   title,
-  minValue,
-  maxValue,
-  ChangePrixMin,
-  ChangePrixMax,
   error,
-  filteredArticles,
-  onClick,
-  onFilter,
-  reset,
+  allValues
 }) => {
+
+  
+  const [prixMin, setPrixMin] = useState("");
+  const [prixMax, setPrixMax] = useState("");
+
+  const handlePrixMin = (e) => setPrixMin(e.target.value);
+  const handlePrixMax = (e) => setPrixMax(e.target.value);
+  
+  const [filteredArticles, setFilteredArticles] = useState(allValues);
+
+  //Quand on utilise un "props" dans le initial value d'une state variable, on est obligé d'utiliser useEffect, pour que la state variable se mette à jour quand le props change, sinon React ne comprend pas
+  useEffect(() => {
+    setFilteredArticles(allValues)
+  }, [allValues])
+  
+  
+  const handleFilter = () => {
+    
+    const minPrice = prixMin
+      ? parseFloat(prixMin.replace(/\s+/g, ""))
+      : -Infinity;
+
+    const maxPrice = prixMax
+      ? parseFloat(prixMax.replace(/\s+/g, ""))
+      : Infinity;
+  
+    const filtered = allValues.filter((e) => {
+      const price = parseFloat(e.prix.replace(/\s+/g, ""));
+      return price >= minPrice && price <= maxPrice;
+    });
+  
+    // Sort filtered articles by price in ascending order
+    const sortedFiltered = filtered.sort((a, b) => {
+      const priceA = parseFloat(a.prix.replace(/\s+/g, ""));
+      const priceB = parseFloat(b.prix.replace(/\s+/g, ""));
+      return priceA - priceB;
+    });
+  
+    setFilteredArticles(sortedFiltered);
+  };
+
+  const handleReset = () => {
+    setPrixMin("");
+    setPrixMax("");
+    setFilteredArticles(allValues)
+  }
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  };
+
+   
+
   return (
     <main className="padding">
       <div className="martian-mono mb-10 text-center text-3xl text-[#FA7A35]">
@@ -25,26 +74,26 @@ const RangeGrid = ({
           type="text"
           className="relative mr-10 w-96 rounded-full border border-gray-400 py-3 pl-12 transition duration-150 ease-in-out hover:shadow-md focus:outline-none focus:ring-1 focus:ring-blue-500 max-lg:pl-10"
           placeholder="Prix minimum"
-          value={minValue}
-          onChange={ChangePrixMin}
+          value={prixMin}
+          onChange={handlePrixMin}
         />
         <input
           type="text"
           className="relative w-96 rounded-full border border-gray-400 py-3 pl-12 transition duration-150 ease-in-out hover:shadow-md focus:outline-none focus:ring-1 focus:ring-blue-500 max-lg:pl-10"
           placeholder="Prix Maximum"
-          value={maxValue}
-          onChange={ChangePrixMax}
+          value={prixMax}
+          onChange={handlePrixMax}
         />
 
         <button
           className="ml-4 rounded-full bg-blue-500 px-4 py-2 text-white transition duration-150 ease-in-out hover:shadow-md"
-          onClick={onFilter}
+          onClick={handleFilter}
         >
-          Appliquer
+         Filtrer
         </button>
         <button
           className="ml-4 rounded-full bg-blue-500 px-4 py-2 text-white transition duration-150 ease-in-out hover:shadow-md"
-          onClick={reset}
+          onClick={handleReset}
         >
           Reset
         </button>
@@ -60,7 +109,7 @@ const RangeGrid = ({
                 <div
                   key={e._id}
                   className=" w-full rounded-lg border border-gray-400 transition-transform hover:translate-y-[-5px] hover:cursor-pointer hover:shadow-custom"
-                  onClick={onClick}
+                  onClick={scrollToTop}
                 >
                   <div className="h-60 w-full max-lg:h-52">
                     <img
