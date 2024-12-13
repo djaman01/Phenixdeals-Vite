34f25bf7da69d8f11c9fac4365506803c64875d0
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-12-13 12:05:51 +0100

    fin new layout card

--- src/components/RangeGrid.jsx
+++ src/components/RangeGrid.jsx
@@ -157,7 +157,7 @@ const RangeGrid = ({ title, error, allValues, showSearchInput, typeObjet }) => {
                     />
                   </div>
                   <div className="h-[167px] text-center text-xl max-lg:h-[186px]">
-                    <h3 className="flex my-1 h-5 items-center justify-center font-mono font-bold text-blue-600 max-lg:text-lg ">
+                    <h3 className="flex my-1 h-4 items-center justify-center font-mono font-bold text-blue-600 max-lg:text-lg ">
                       {e.type}
                     </h3>
                     <div className="mx-auto w-1/2 border-b border-gray-300"></div>
