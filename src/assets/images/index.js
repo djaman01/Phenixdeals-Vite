29d5979fe281f89723b3b9ec937de62bbd5fec13
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2025-01-17 13:23:21 +0100

    chgt logo et affiche

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,7 @@
-import logoPhenix from "./phenix-nobg.png"
+import logoPhenix from "./logo-phenix-nobg.png"
 import phenixAchat from "./phenix-achat.jpg"
 import phenixVendre from "./phenix-vendre.jpg"
-import phenixPub from "./phenix-pub.jpg"
+import phenixPub from "./phenix-deals.jpg"
 import gbouriArtFirst from "./Gbouri-ArtFirst.jpg"
 
 export {logoPhenix, phenixAchat, phenixVendre, phenixPub, gbouriArtFirst}
