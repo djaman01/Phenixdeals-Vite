26b3439d7c23b9bd027eddec0b465b90b3e639a7
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2025-06-10 23:13:02 +0100

    ajout rules eslin pour enlevr signal erreur

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/no-unescaped-entities": "off", //Pour enlever les soulignements rouges quand j'écris des apostrophes
+    "react/prop-types": "off", //Pour enlever les soulignements rouges dans les props
   },
   globals: {
-    require: 'readonly', // Reconnaître 'require' comme une variable globale en lecture seule
+    require: "readonly", // Reconnaître 'require' comme une variable globale en lecture seule
   },
 };
