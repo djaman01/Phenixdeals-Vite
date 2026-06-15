46d4e66c096bd9a9c393b2f83b54d6424c00d6eb
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2026-06-15 10:46:51 +0100

    chgt header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,25 +29,16 @@ const Header = () => {
               Accueil
             </p>
           </Link>
-          <div className="group relative">
-            <p className="cursor-pointer rounded-lg px-2 py-1 text-xl hover:bg-[#e4e9f1] ">
+          <Link to="/oeuvres">
+            <p className="cursor-pointer rounded-lg px-2 py-1 text-xl hover:bg-[#e4e9f1] active:rounded-lg active:bg-[#3b82f6] active:px-2 active:py-1 active:text-white">
               Oeuvres
             </p>
-
-            <div className="font-roboto absolute z-10 hidden w-52 cursor-pointer rounded-lg bg-white font-serif text-gray-700 shadow-xl ring-1 ring-slate-900/5 group-hover:block">
-              <Link to="/allArtists">
-                <p className="mb-3 mt-2 py-1 pl-2 hover:bg-[#e4e9f1] active:bg-[#3b82f6] active:text-white">
-                  Tous les Artistes
-                </p>
-              </Link>
-              <Link to="/oeuvres">
-                <p className="mb-2 py-1 pl-2 hover:bg-[#e4e9f1] active:bg-[#3b82f6] active:text-white ">
-                  Toutes les oeuvres
-                </p>
-              </Link>
-            </div>
-          </div>
-
+          </Link>
+          <Link to="/allArtists">
+            <p className="cursor-pointer rounded-lg px-2 py-1 text-xl hover:bg-[#e4e9f1] active:rounded-lg active:bg-[#3b82f6] active:px-2 active:py-1 active:text-white">
+              Artistes
+            </p>
+          </Link>
           <Link to="/bestDeals">
             <p className="cursor-pointer rounded-lg bg-gradient-to-r from-[#B5121B] via-[#FA7A35] to-[#F7C331] bg-clip-text px-2 py-1 text-center text-xl text-transparent active:text-[#B5121B] ">
               Best Deals
